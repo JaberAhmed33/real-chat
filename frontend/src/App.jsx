94c168bec1690a1f56269aa79b66d77bf9f19b62
JaberAhmed33 <jaber33357@gmail.com>
2024-03-01 01:10:37 +0200

    update random background

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useLayoutEffect, useState } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
@@ -7,19 +7,19 @@ import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext";
 
 function App() {
-  const [bg, setBg] = useState("/bg1.webp");
+  const bgRef = useRef();
 
   const { authUser } = useAuthContext();
 
   useLayoutEffect(() => {
     const newBg = "/bg" + (Math.floor(Math.random() * 7) + 1) + ".webp";
-
-    setBg(newBg);
+    bgRef.current.style.backgroundImage = `url(${newBg})`
   }, []);
 
   return (
     <main
-      className={`w-full relative min-h-screen bg-[url('${bg}')] bg-fixed bg-center z-0 bg-no-repeat	bg-cover after:absolute after:top-0 after:z-[-1] after:w-full after:h-full after:bg-gradient-to-r from-gray-900/20 to-gray-900/20`}
+      className={`w-full relative min-h-screen bg-fixed bg-center z-0 bg-no-repeat	bg-cover after:absolute after:top-0 after:z-[-1] after:w-full after:h-full after:bg-gradient-to-r from-gray-900/20 to-gray-900/20`}
+      ref={bgRef}
     >
       <section className="p-4 h-screen flex items-center justify-center">
         <Routes>
