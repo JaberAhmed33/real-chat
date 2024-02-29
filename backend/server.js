c95c08e4cfbee3d06e0ca6b3c4f0bae871ac2089
JaberAhmed33 <jaber33357@gmail.com>
2024-03-01 00:37:43 +0200

    error update

--- backend/server.js
+++ backend/server.js
@@ -34,10 +34,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", usersRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.use(express.static(path.join(__dirname, "/backend/dist")))
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/backend/dist/index.html"))
+  res.sendFile(path.join(__dirname, "/frontend/dist/index.html"))
 })
 
 server.listen(port, () => {
