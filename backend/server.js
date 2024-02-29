bc7e9bdf42abd9f783a0af35580b88f02eebfb82
JaberAhmed33 <jaber33357@gmail.com>
2024-03-01 00:47:46 +0200

    update error sever

--- backend/server.js
+++ backend/server.js
@@ -28,17 +28,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => res.send("بسم الله الرحمن الرحيم"));
-
 app.use("/api/auth", authRoutes);
 app.use("/api/users", usersRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")))
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/frontend/dist/index.html"))
-})
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(port, () => {
   connectToDB();
