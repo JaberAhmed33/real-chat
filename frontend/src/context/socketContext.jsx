bbe1c255035cf445e04cb0f70d35596eb085fddd
Jaber Ahmed <70724851+JaberAhmed33@users.noreply.github.com>
2024-03-01 01:28:13 +0200

    Update socketContext.jsx

--- frontend/src/context/socketContext.jsx
+++ frontend/src/context/socketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:7000", {
+      const socket = io("https://real-chat-kh45.onrender.com", {
         query: {
           userId: authUser._id,
         },
